21969739f7a638b981d5a83d26612633319ff4f7
Calum <calum@paceup.com>
2020-04-01 16:46:28 -0700

    Lesson 3.1

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,11 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("<h1>Blog home</h1>")
+    return render(request, 'blog/home.html')
 
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, 'blog/about.html')
