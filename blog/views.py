8a920182d8cad906569030b3b98521804d79ab8c
Calum <calum@paceup.com>
2020-04-01 17:12:36 -0700

    Lesson 3

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+posts = [
+    {
+        "author": "Calum",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "August 27th 2020",
+    },
+    {
+        "author": "John",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "August 28th 2020",
+    },
+]
 
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    context = {"title": "About"}
+    return render(request, "blog/about.html", context)
