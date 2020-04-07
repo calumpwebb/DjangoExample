0e82af3668603e02ae2014fd84fa0ac26b2d0d36
Calum <calum@paceup.com>
2020-04-06 20:47:06 -0700

    Lesson 9 + Black

--- users/forms.py
+++ users/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from users.models import Profile
 
+
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
@@ -10,14 +11,16 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ["username", "email", "password1", "password2"]
 
+
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ["username", "email"]
+
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['image']
+        fields = ["image"]
